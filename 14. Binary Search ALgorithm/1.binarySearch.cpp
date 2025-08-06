aa0f7cb7b8514fa040a98300f6f61cc6efc268ca
Jonayed <jonayed@Jonayeds-MacBook-Pro.local>
2025-08-06 23:23:05 +0600

    binary search implemented

--- 14. Binary Search ALgorithm/1.binarySearch.cpp
+++ 14. Binary Search ALgorithm/1.binarySearch.cpp
@@ -1,8 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+
+int findNumber(vector<int> &num, int target)
+{
+    int last = num.size() - 1;
+    int first = 0;
+    while (first <= last)
+    {
+        int mid = (last + first) / 2;
+        if (num[mid] == target)
+        {
+            return mid;
+        }
+        else if (target > num[mid])
+        {
+            first = mid + 1;
+        }
+        else if (target < num[mid])
+        {
+            last = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
     // real life binary search example is searching word in a dictionary
     // it only applicable only on sorted array
-    
 
+    vector<int> numbers = {1, 2, 3, 4, 5};
+    int target;
+    cin >> target;
+    int result = findNumber(numbers, target);
+
+    if (result != -1)
+    {
+        cout << "Target found at index: " << result << endl;
+    }
+    else
+    {
+        cout << "Target not found" << endl;
+    }
 }
