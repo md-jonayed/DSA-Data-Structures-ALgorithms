d067a78a088342015a89fc45724fa135ba560d4a
Jonayed <jonayed@Jonayeds-MacBook-Pro.local>
2025-08-07 22:16:47 +0600

    binary search optimized mid calculation

---
+++ 14. Binary Search ALgorithm/3.OptimizedMidBinarySearch.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findNumber(vector<int> &num, int target)
+{
+    int last = num.size() - 1;
+    int first = 0;
+    while (first <= last)
+    {
+        int mid = first + (last - first) / 2;
+        if (num[mid] == target)
+        {
+            return mid;
+        }
+        else if (target > num[mid])
+        {
+            first = mid + 1;
+        }
+        else if (target < num[mid])
+        {
+            last = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> numbers = {1, 2, 3, 4, 5};
+    int target;
+    cin >> target;
+    int result = findNumber(numbers, target);
+
+    if (result != -1)
+    {
+        cout << "Target found at index: " << result << endl;
+    }
+    else
+    {
+        cout << "Target not found" << endl;
+    }
+}
+
+// binary search time complexity = O(log n)
