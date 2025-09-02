55ede13044af35d40c6e37ce36583cbd68f56db4
Jonayed <jonayed@Jonayeds-MacBook-Pro.local>
2025-09-02 23:53:56 +0600

    insertion sort done

---
+++ 15. Sorting Algorithms/4.insertionSort.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> nums = {4, 1, 5, 2, 3};
+    for (int i = 1; i < nums.size();i++){
+        int key = nums[i];
+        int j = i - 1;
+        while(j>=0 && nums[j]>key){
+            nums[j + 1] = nums[j];
+            j--;
+        }
+        nums[j + 1] = key;
+    }
+
+    for(int n:nums){
+        cout << n <<",";
+    }
+    cout << endl;
+}
